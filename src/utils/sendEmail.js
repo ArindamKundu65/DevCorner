5af461e97bbd0478653649bdc242fd440a8e5c9f
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-30 19:58:33 +0530

    using resend api for email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,47 +1,27 @@
 import "dotenv/config";
+import { Resend } from "resend";
 
-
-import nodemailer from "nodemailer";
-
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: false, // STARTTLS
-    family: 4,      // Force IPv4
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.log("Transport error:", error);
-    } else {
-        console.log("Transport is ready");
-    }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendConnectionRequestEmail = async (
-    receiverEmail,
-    receiverName,
-    senderName
+  receiverEmail,
+  receiverName,
+  senderName
 ) => {
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: receiverEmail,
-        subject: "You have a new connection request",
-        html: `
-            <h2>Hello ${receiverName},</h2>
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: receiverEmail,
+    subject: "You have a new connection request",
+    html: `
+      <h2>Hello ${receiverName},</h2>
 
-            <p>
-                <strong>${senderName}</strong> has sent you a connection request
-                on DevCorner.
-            </p>
+      <p>
+        <strong>${senderName}</strong> has sent you a connection request on DevCorner.
+      </p>
 
-            <p>Log in to DevCorner to view the request.</p>
-        `
-    });
-   
+      <p>Log in to DevCorner to view the request.</p>
+    `,
+  });
+
+  console.log(response);
 };
