16e790d405b53e87a8091be4337237e383cd5ac1
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-31 23:09:48 +0530

    d

--- src/routes/user.js
+++ src/routes/user.js
@@ -70,46 +70,35 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
     try {
-
         const loggedInUser = req.user;
 
-        const page = parseInt(req.query.page) || 1;
-        let limit = parseInt(req.query.limit) || 10;
-        limit = limit > 50 ? 50 : limit;
-
-        const skip = (page -1) * limit;
-
-
         const connectionRequests = await ConnectionRequest.find({
-            $or: [{ fromUserId: loggedInUser._id}, { toUserId: loggedInUser._id}],
-        }).select("fromUserId  toUserId");
-
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id },
+            ],
+        }).select("fromUserId toUserId");
 
         const hideUsersFromFeed = new Set();
-        connectionRequests.forEach((req) => {
-            
-            hideUsersFromFeed.add(req.fromUserId.toString());
-            hideUsersFromFeed.add(req.toUserId.toString());
-            
+
+        connectionRequests.forEach((request) => {
+            hideUsersFromFeed.add(request.fromUserId.toString());
+            hideUsersFromFeed.add(request.toUserId.toString());
         });
 
+        // Hide the logged-in user as well
+        hideUsersFromFeed.add(loggedInUser._id.toString());
+
         const users = await User.find({
-            $and: [
-               { _id: { $nin: Array.from(hideUsersFromFeed) }},
-               { _id: { $ne: loggedInUser._id } }
+            _id: { $nin: [...hideUsersFromFeed] },
+        })
+            .select(USER_SAFE_DATA)
+            .limit(10);
 
-            ]
-           
-        }).select(USER_SAFE_DATA)
-        .skip(skip)
-        .limit(limit);
-
-
-        res.send(users)
+        res.send(users);
     } catch (error) {
-        res.status(400).send("ERROR: "+error.message);
+        res.status(400).send("ERROR: " + error.message);
     }
-})
-
+});
 
 export default userRouter
