ac5291fe3866d2fe13b7e5ed5ae642dd48d154da
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 20:02:03 +0530

    changed cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const port = process.env.PORT
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://dev-front-ruby.vercel.app",
+    origin: ["https://dev-front-ruby.vercel.app","http://localhost:5173" ],
     credentials: true
 }));
 app.use("/",authRouter);
