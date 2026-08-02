185d5c88f0db6ab750587fa86e6df069f669a6e5
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-02 19:21:01 +0530

    socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 
 
 const initializeSocket = (server) => {
+    console.log("initializeSocket called");
 
     const io = new Server(server, {
         cors: {
@@ -12,6 +13,20 @@ const initializeSocket = (server) => {
     });
 
     io.on("connection", (socket) => {
+        socket.on("joinChat", ({ userId, targetUserId }) => {
+            const roomId = [userId, targetUserId].join("_");
+            console.log("joining room: " + roomId)
+            socket.join(roomId);
+
+        });
+
+        socket.on("sendMessage", () => {
+
+        });
+
+        socket.on("disconnect", ()=> {
+            
+        })
 
     });
 
