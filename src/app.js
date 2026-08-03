26b0452aeee26d96d84e3972bf4b2dde9dab734a
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-03 18:36:39 +0530

    deploy

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const port = process.env.PORT
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: ["https://devcorner-jqtp.onrender.com","http://localhost:5173" ],
+    origin: ["https://dev-front-ruby.vercel.app","http://localhost:5173" ],
     credentials: true
 }));
 app.use("/",authRouter);
